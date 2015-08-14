9cf2079dd2d32bf617eca7a9ccf70fcfeb477970
Ryan Done <rbdone@gmail.com>
2015-08-13 22:31:22 -0600

    initial commit, fork from ng-hackathon-starter

---
+++ src/main.ts
@@ -0,0 +1,46 @@
+import 'reflect-metadata';
+import 'es6-shim';
+import 'zone.js';
+
+import {Component, View, bootstrap, NgFor} from 'angular2/angular2';
+import {httpInjectables} from 'angular2/http';
+import {HashLocationStrategy, LocationStrategy, Router, RouterLink, RouteConfig, RouterOutlet, routerInjectables} from 'angular2/router';
+import {bind, Injectable} from 'angular2/di';
+import {EventEmitter, ObservableWrapper} from 'angular2/src/facade/async';
+
+
+import Home from './home';
+import RepoList from './repo-list';
+import WeatherApp from './WeatherApp';
+
+@RouteConfig([
+    {path: '/', as: 'home', component:Home},
+    {path: '/repo-list', as: 'repo-list', component:RepoList},
+    {path: '/weather', as: 'weather', component:WeatherApp},
+])
+@Component({
+    selector: 'app'
+})
+@View({
+    directives: [RouterOutlet, RouterLink],
+    template: `
+        <nav>
+            <a [router-link]="['/home']">Home</a>
+            <a [router-link]="['/repo-list']">Repo List</a>
+            <a [router-link]="['/weather']">WeatherApp</a>
+        </nav>
+        <main>
+            <router-outlet></router-outlet>
+        </main>
+    `
+})
+class App {}
+
+bootstrap(App, [
+    httpInjectables,
+    routerInjectables,
+    bind(LocationStrategy).toClass(HashLocationStrategy)
+]).then(
+        success => console.log(`Bootstrap success`),
+        error => console.log(error)
+);
