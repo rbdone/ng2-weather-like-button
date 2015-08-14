c1844826ec382a16e785e08fa6686e4e3a82391e
Ryan Done <rbdone@gmail.com>
2015-08-14 10:09:27 -0600

    changes

--- src/home.ts
+++ src/home.ts
@@ -6,13 +6,15 @@ import LikeIt from "./like-it";
 @Component({
     selector: "home"
 })
-@Animations("like-it-animations.json")
+//@Animations("like-it-animations.json")
 @View({
     directives: [formDirectives, LikeIt],
     //I could've just onButtonClick(name), but wanted to show #input syntax
     template:`
         <h1>Like it... Like it good!</h1>
         <like-it #likeit></like-it> <h1 style="font-size:128px;">{{likeit.count}}</h1>
+        <like-it #bob></like-it> <h1 style="font-size:128px;">{{bob.count}}</h1>
+        <button (click)="bob.clickIt()">count more</button>
     `
 })
 export default class Home{
--- src/like-it.ts
+++ src/like-it.ts
@@ -1,11 +1,14 @@
-import {NgFor, Component, View, bootstrap} from "angular2/angular2";
+import {Component, View, bootstrap} from "angular2/angular2";
 
 @Component({
     selector: 'like-it'
 })
 @View({
     template: `
+    <div>
         <img (click)="clickIt()" src="./googleLike.jpeg" width="200px" />
+        <span style="color:red;">{{count}}</span>
+    </div>
     `
 })
 export default class LikeIt {
